fb441c250cec864a49d7d681524e099e12ed3e9a
Magnus Ulimoen <magnusu@met.no>
2025-03-20 08:21:19 +0100

    debug

--- mapproxy/cache/mbtiles.py
+++ mapproxy/cache/mbtiles.py
@@ -64,7 +64,7 @@ class MBTilesCache(TileCacheBase):
     def db(self):
         if not getattr(self._db_conn_cache, 'db', None):
             self.ensure_mbtile()
-            self._db_conn_cache.db = sqlite3.connect(self.mbtile_file, self.timeout)
+            self._db_conn_cache.db = sqlite3.connect(f"file:{self.mbtile_file}?mode=ro", self.timeout)
         return self._db_conn_cache.db
 
     def cleanup(self):
